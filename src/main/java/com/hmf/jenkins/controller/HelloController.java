f766ddb3f438628d740123237545297f7b1de255
hmf <1335114917@qq.com>
2022-04-09 12:06:03 +0800

    3.再次自动构建测试

--- src/main/java/com/hmf/jenkins/controller/HelloController.java
+++ src/main/java/com/hmf/jenkins/controller/HelloController.java
@@ -9,8 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/data")
-    public String hello() {
+    public String data() {
         System.out.println("--------------- data --------------");
         return "Hello My Cruel World !!!";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        System.out.println("--------------- hello --------------");
+        return "Hello My Cruel World !!! Again !!!";
     }
 }
